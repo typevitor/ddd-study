5ab51c3e8788df97d80b0e25fb78fd8364e553d7
Vitor Leal <vitorlealeng@gmail.com>
2025-05-01 10:02:41 -0300

    order item tests

--- src/entities/order_item.ts
+++ src/entities/order_item.ts
@@ -1,14 +1,32 @@
 export class OrderItem {
-  id: string;
-  product: string;
-  quantity: number;
-  price: number;
+   
+  private id: string;
+  private productId: string;
+  private quantity: number;
+  private price: number;
 
-  constructor(id: string, product: string, quantity: number, price: number) {
+  constructor(id: string, productId: string, quantity: number, price: number) {
     this.id = id;
-    this.product = product;
+    this.productId = productId;
     this.quantity = quantity;
     this.price = price;
+
+    this.validate();
+  }
+
+  private validate() {
+    if (!this.id) {
+      throw new Error('ID is required');
+    }
+    if (!this.productId) {
+      throw new Error('Product ID is required');
+    }
+    if (this.quantity <= 0) {
+      throw new Error('Quantity must be greater than zero');
+    }
+    if (this.price < 0) {
+      throw new Error('Price must be greater than zero');
+    }
   }
 
   getTotalPrice(): number {
--- tests/entities/order.spec.ts
+++ tests/entities/order.spec.ts
@@ -42,4 +42,23 @@ describe('OrderUnitTest', () => {
     order.addItem(new OrderItem('2', 'Item 2', 50, 1));
     expect(order.calculateTotal()).toBe(250);
   });
+
+  it('should throw error if item quantity is less or equal than zero', () => {
+    const order = new Order('1', '123', new Date(), [
+      new OrderItem('1', 'Item 1', 100, 2),
+    ]);
+    expect(() => {
+      order.addItem(new OrderItem('2', 'Item 2',-1, 10));
+      order.addItem(new OrderItem('2', 'Item 2', 0, 20));
+    }).toThrow('Quantity must be greater than zero');
+  });
+
+  it('should throw error if item price is less than zero', () => {
+    const order = new Order('1', '123', new Date(), [
+      new OrderItem('1', 'Item 1', 100, 2),
+    ]);
+    expect(() => {
+      order.addItem(new OrderItem('2', 'Item 2', 1, -50));
+    }).toThrow('Price must be greater than zero');
+  });
 });
