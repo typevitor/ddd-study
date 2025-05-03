d8f2f878c2c1d8934ef0fcaff4adeb595ed4f9c0
Vitor Leal <vitorlealeng@gmail.com>
2025-05-03 10:05:11 -0300

    product service

---
+++ src/service/product.service.ts
@@ -0,0 +1,11 @@
+import { Product } from "../entities/product";
+
+export class ProductService {
+
+  static increasePrice(products: Product[], percentage: number): void {
+    products.forEach((product) => {
+      const newPrice = product.getPrice() * (1 + percentage / 100);
+      product.changePrice(Math.floor(newPrice));
+    });
+  }
+}
---
+++ tests/service/product.service.spec.ts
@@ -0,0 +1,19 @@
+import { Product } from "../../src/entities/product";
+import { ProductService } from "../../src/service/product.service";
+
+describe('ProductServiceUnitTest', () => {
+
+  it('should change the prices of all products', () => {
+    const product1 = new Product('1', 'Product 1', 100);
+    const product2 = new Product('2', 'Product 2', 200);
+    const product3 = new Product('3', 'Product 3', 300);
+
+    ProductService.increasePrice([product1, product2, product3], 10);
+
+    expect(product1.getPrice()).toBe(110);
+    expect(product2.getPrice()).toBe(220);
+    expect(product3.getPrice()).toBe(330);
+  
+  });
+
+});
