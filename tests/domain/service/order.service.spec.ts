83f8b0a26869661199f37f977ac01489a9a8fe74
Vitor Augusto <vitorlealeng@gmail.com>
2025-05-19 07:34:17 -0300

    move service tests to domain folder

--- tests/domain/service/order.service.spec.ts
+++ tests/domain/service/order.service.spec.ts
@@ -1,7 +1,7 @@
-import { Order } from "../../src/domain/entity/order";
-import { OrderItem } from "../../src/domain/entity/order_item";
-import { OrderService } from "../../src/domain/service/order.service";
-import { Customer } from "../../src/domain/entity/customer";
+import { Order } from "../../../src/domain/entity/order";
+import { OrderItem } from "../../../src/domain/entity/order_item";
+import { OrderService } from "../../../src/domain/service/order.service";
+import { Customer } from "../../../src/domain/entity/customer";
 
 describe('OrderServiceUnitTest', () => {
 
